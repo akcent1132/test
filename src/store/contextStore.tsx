0f28fdc922da697ac352721221ea1d41ac5d4cca
Akcent1132 <grom1132@mail.ru>
2024-08-07 15:31:13 +0300

    feat: state managing by context

---
+++ src/store/contextStore.tsx
@@ -0,0 +1,55 @@
+import React, {
+    createContext,
+    FC,
+    PropsWithChildren,
+    useContext,
+    useEffect,
+    useState,
+  } from 'react';
+import { TJobData, TParamData, TPersonalData } from '../utils/types';
+
+  export const StoreContext = createContext(undefined);
+  type Props = {
+    children?: React.ReactNode
+  };
+export const StoreProvider : React.FC<Props> = ({children}: Props): React.ReactNode => {
+    const [personalData,setPersonalData] = useState<TPersonalData | null>(null);
+    const [paramData,setParamData] = useState<TParamData | null>(null);
+    const [jobData,setJobData] = useState<TJobData | null>(null);
+    const [jobList,setJobList] = useState(null);
+    const applyParamData = (data:TParamData) => {
+      setParamData(data);
+    }
+    const applyJobData = (data:TJobData) => {
+      setJobData(data);
+    }
+    const ApplyFormData = (form:TPersonalData) => {
+        setPersonalData(form)
+    }
+    const getJobList = () => {
+      fetch('https://dummyjson.com/products/category-list')
+       .then(res => res.json())
+        .then(list => setJobList(list))
+        .catch((e)=> console.error(e));
+    }
+    useEffect(() => {
+      getJobList()
+    }, [])
+return (<StoreContext.Provider value={{ personalData,jobData,paramData,jobList,ApplyFormData,applyJobData,applyParamData }}>
+    {children}
+    </StoreContext.Provider>
+);
+}
+
+
+export const useStorage = () => {
+    const ctx = useContext(StoreContext);
+  
+    if (!ctx) {
+      throw new Error(
+        'useStorage should be used inside Context',
+      );
+    }
+  
+    return ctx;
+  };
